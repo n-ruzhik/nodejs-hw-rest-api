a3838830d3e9da63975ce1e9a9a2f659a15f93c8
n-ruzhik <anastasiyaruzhetskaya@gmail.com>
2023-07-02 21:04:59 +0300

    add mongodb

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
-const app = require('./app')
+// 36u7CkPSSlA7u02f
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require("mongoose");
+
+const app = require("./app");
+
+const DB_HOST =
+  "mongodb+srv://Nastya:36u7CkPSSlA7u02f@cluster0.z8d4yi0.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(3000, () => {
+      console.log("Database connection successful");
+    })
+  )
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
