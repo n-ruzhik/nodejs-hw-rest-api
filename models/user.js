87cc42d592fd42fa1b4cd37129824788a60a07b5
n-ruzhik <anastasiyaruzhetskaya@gmail.com>
2023-07-09 22:10:10 +0300

    add authentication elements

---
+++ models/user.js
@@ -0,0 +1,58 @@
+const { Schema, model } = require("mongoose");
+const joi = require("joi");
+
+const { handleMongooseError } = require("../helpers");
+
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+      minlength: 6,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      match: emailRegexp,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: "",
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleMongooseError);
+
+const registerSchema = joi.object({
+  password: joi.string().min(6).required(),
+  email: joi.string().pattern(emailRegexp).required(),
+  //   subscription: joi.string().required(),
+});
+
+const loginSchema = joi.object({
+  password: joi.string().min(6).required(),
+  email: joi.string().pattern(emailRegexp).required(),
+  //   subscription: joi.string().required(),
+});
+
+const schemas = {
+  registerSchema,
+  loginSchema,
+};
+
+const User = model("user", userSchema);
+
+module.exports = {
+  User,
+  schemas,
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,32 +2,22 @@ const express = require("express");
 
 const ctrl = require("../../controllers/contacts");
 
-const { validateBody, isValidId } = require("../../middlewares");
+const { validateBody, isValidId, authenticate } = require("../../middlewares");
 
 const { schemas } = require("../../models/contact");
 
 const router = express.Router();
 
-router.get("/", ctrl.getAll);
+router.get("/", authenticate, ctrl.getAll);
 
-router.get("/:contactId", isValidId, ctrl.getById);
+router.get("/:contactId", authenticate, isValidId, ctrl.getById);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
+router.post("/", authenticate, validateBody(schemas.addSchema), ctrl.addContact);
 
-router.put(
-  "/:contactId",
-  isValidId,
-  validateBody(schemas.addSchema),
-  ctrl.updateContact
-);
+router.put("/:contactId", authenticate, isValidId, validateBody(schemas.addSchema), ctrl.updateContact);
 
-router.patch(
-  "/:contactId/favorite",
-  isValidId,
-  validateBody(schemas.updateFavoriteSchema),
-  ctrl.updateFavorite
-);
+router.patch("/:contactId/favorite", authenticate, isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateFavorite);
 
-router.delete("/:contactId", isValidId, ctrl.deleteContact);
+router.delete("/:contactId", authenticate, isValidId, ctrl.deleteContact);
 
 module.exports = router;
